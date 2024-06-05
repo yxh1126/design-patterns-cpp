5dac2f487e4bb3f2aa22b2354794cfca3f4bbcff
Yi Huang <yi.huang@inceptioglobal.ai>
2024-06-05 14:21:10 -0700

    update

--- interview/questions/data_frame.cc
+++ interview/questions/data_frame.cc
@@ -12,15 +12,18 @@ packets that together form a full message:
 ------------------------------------------------------------------------------------
       0            2            0              1           0x1122         0
       0            0            1              0           0x3344         0
-Example full-file name (“Data Frame” field) received over 2 packets
-(ASCII-encoded): 0x11223344
+
+Example full-file name (“Data Frame” field) received over 2 packets (ASCII-encoded):
+0x11223344
 
 On the first packet of a message received, bits 27-29 will indicate the number
 of packets that will be in the complete request message. In the following packets
 for this message these bits will be cleared. Likewise, bits 20-23 of the first
 packet will indicate the permissions to create the file with:
+
 0000 - Read-only
-0001- Read-write
+0001 - Read-write
+
 The aggregation of the data frames will dictate the filename to create on disk.
 Each letter of the final filename will be encoded in ASCII in the data frame.
 The least significant bit is the 0th bit.
